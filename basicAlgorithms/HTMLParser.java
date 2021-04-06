79aed109bc84f02a266c46e69eba5787ffe22ad1
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-04-06 09:52:39 +0530

    html parser

---
+++ basicAlgorithms/HTMLParser.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+class HTMLParser(String html)
+{
+        Stack<String> buffer = new Stack<>();
+        int j = html.indexOf('<');
+        while(j != -1)
+        {
+                int k = html.indexOf(j+1 , '>');
+                if(k == -1)
+                        return false;
+                String tag = html.substring(j+1 , k);
+                if(!tag.startsWith("/"))
+                        stack.push(tag);
+                else 
+                {
+                        if(stack.isEmpty())
+                                return false;
+                        if(!tag.substring(1).equals(stack.pop()))
+                                return false;
+                }
+                j = html.indexOf('<' , k+1);
+        }
+        return buffer.isEmpty();
+}
+
+
