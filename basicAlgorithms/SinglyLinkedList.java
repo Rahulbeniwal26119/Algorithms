d65f5a563fc849aaed36fe7446e33192c52d8ec6
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-04-07 08:21:07 +0530

    adding singlyLinkedList for LinkedQueue and LinkedStack

---
+++ basicAlgorithms/SinglyLinkedList.java
@@ -0,0 +1,75 @@
+class SinglyLinkedList<E>
+{
+        private static class Node<E> 
+        {
+                private E element;
+                private Node<E> next;
+         
+                public Node(E d , Node<E> n)
+                {
+                        element = d;
+                        next  = n;
+                }
+
+                public E getElement() 
+                {
+                        return element;
+                }
+
+                public Node<E> getNext()
+                {
+                        return next;
+                }
+
+                public void setNext(Node <E> n)
+                {
+                        next = n;
+                }
+        }
+        private Node<E> head=null;
+        int size=0;
+        private Node<E> tail=null;
+        public SinglyLinkedList() {}
+        public int size() { return size;}
+        public boolean isEmpty() { return size==0; }
+        public E first()
+        {
+                if(size == 0) return null;
+                return head.getElement();
+        }
+        public E last()
+        {
+                if(isEmpty()) return null;
+                return tail.getElement();
+        }
+
+        public void addFirst(E data)
+        {
+                 head = new Node(data , head);
+                 if(size == 0)
+                         tail = head;
+                 size++;
+        }
+
+        public void addLast(E data)
+        {
+                Node newNode = new Node<>(data , null);
+                if(isEmpty()) 
+                        head = newNode;
+                else 
+                        tail.setNext(newNode);
+                tail = newNode;
+                size++;
+        }
+
+        public E removeFirst()
+        {
+                if(isEmpty()) return null;
+                E answer = head.getElement();
+                head = head.getNext();
+                size--;
+                if(size == 0)
+                         tail = null;
+                return answer;
+        }
+}
