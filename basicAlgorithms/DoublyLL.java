ee98fc10b37f0e82a5da522b1d1b3ae3380d4209
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-04-07 08:18:20 +0530

    changing its implementation

--- basicAlgorithms/DoublyLL.java
+++ basicAlgorithms/DoublyLL.java
@@ -1,84 +1,101 @@
-public class DoublyLL<E>
-{
-	private static class Node<E> 
-	{
-		private E data;
-		private Node<E> next;
+public class DoublyLL<E> {
+	private static class Node<E> {
+		private E element;
 		private Node<E> prev;
-		public Node(E e, Node<E> p, Node<E> n)
-		{
-			data = e;
+		private Node<E> next;
+
+		private Node(E e, Node<E> p, Node<E> n) {
+			element = e;
+			prev = p;
 			next = n;
+		}
+
+		public E getElement() {
+			return element;
+		}
+
+		public Node<E> getPrev() {
+			return prev;
+		}
+
+		public Node<E> getNext() {
+			return next;
+		}
+
+		public void setNext(Node<E> n) {
+			next = n;
+		}
+
+		public void setPrev(Node<E> p) {
 			prev = p;
 		}
 
-		public E getData() {return data;}
-		public Node<E> getPrev() { return prev;}
-		public Node<E> getNext() { return next;}
-		public void setPrev(Node<E> p) { prev = p;}
-		public void setNext(Node<E> n) { next = n;}
 	}
+
 	private Node<E> header;
 	private Node<E> trailer;
-	private int size=0;
-	public DoublyLL(){
-		header = new Node<>(null , null , null); // prev is not defined till now 	
-		trailer = new Node<>(null , header , null);
+
+	private int size = 0;
+
+	public DoublyLL() {
+		header = new Node<>(null, null, null);
+		trailer = new Node<>(null, header, null);
 		header.setNext(trailer);
 	}
-	public int size() { return size;}
-	public isEmpty() { return size == 0; }
-	public E first()
-	{
-		if(isEmpty())
+
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public E first() {
+		if (isEmpty())
 			return null;
-		return header.getNext().getData();
+		return header.getNext().getElement();
 	}
-	public E last()
-	{
-		if(isEmpty()) return null;
-		return trailer.getPrev().getData();
+
+	public E last() {
+		if (isEmpty())
+			return null;
+		return trailer.getPrev().getElement();
 	}
-	public void addFirst(E e) 
-	{
-		addBetween(e , header , header.getNext());
+
+	public void addBetween(E e, Node<E> pred, Node<E> succ) {
+		Node<E> newNode = new Node<>(e, pred, succ);
+		pred.setNext(newNode);
+		succ.setPrev(newNode);
+		size++;
 	}
-	public void addLast(E e)
-	{
-		addBeteen(e , trailer.getPrev(), trailer);
+
+	public void addFirst(E e) {
+		addBetween(e, header, header.getNext());
 	}
-	public E removeFirst()
-	{
-		if(isEmpty())
+
+	public void addLast(E e) {
+		addBetween(e, trailer.getPrev(), trailer);
+	}
+
+	public E removeFirst() {
+		if (isEmpty())
 			return null;
 		return remove(header.getNext());
 	}
-	public E removeLast()
-	{
-		if(isEmpty())
+
+	public E removeLast() {
+		if (isEmpty())
 			return null;
 		return remove(trailer.getPrev());
 	}
-	public void addBetween(E e, Node<E> pred , Node<E> succ)
-	{
-		Node<E> newNode = new Node<>(e , pred , succ);
-		pred.setNext(newNOde);
-		succ.getPrev(newNode);
-		size++;
-	}
-	private E remove(Node <E> node)
-	{
-		Node<E> pred = node.prev;
-		Node<E> succ = node.succ;
+
+	public E remove(Node<E> node) {
+		Node<E> pred = node.getPrev();
+		Node<E> succ = node.getNext();
 		pred.setNext(succ);
 		succ.setPrev(pred);
-		--size;
+		size--;
 		return node.getElement();
 	}
-
-
-		pred.setNext(
-
 }
-
-
