56d2cddfa0f04a4519a9fadc09593fc3a6ae46e5
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-04-10 22:40:53 +0530

    Calculate the space of an directory

---
+++ basicAlgorithms/DiskUsage.java
@@ -0,0 +1,23 @@
+import java.io.File;
+public class DiskUsage
+{
+        public static long diskUsage(File root)
+        {
+                long total = root.length();
+                if(root.isDirectory())
+                {
+                        for(String childName : root.list())
+                        {
+                                File child = new File(root , childName);
+                                total += diskUsage(child);
+                        }
+                }
+                System.out.println(total + "\t" +  root);
+                return total;
+        }
+        public static void main(String []args)
+        {
+                diskUsage(new File("/home/rahul/Downloads"));
+        }
+}
+
