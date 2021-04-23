3234e302c1e32bb7cc6e85943b7162093dab916f
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-04-23 08:58:08 +0530

    adding methods like totalInternalNodes, totalExternalNodes, search in bst

--- basicAlgorithms/BST.java
+++ basicAlgorithms/BST.java
@@ -1,19 +1,17 @@
-class Node
-{
-    int  data;
+class Node {
+    int data;
     Node left;
     Node right;
     Node childOf;
 
-    public Node(int  data, Node left, Node right, Node parent)
-    {
+    public Node(int data, Node left, Node right, Node parent) {
         this.data = data;
         this.left = left;
         this.right = right;
         this.childOf = parent;
     }
 
-    public int  getValue() {
+    public int getValue() {
         return data;
     }
 
@@ -45,97 +43,111 @@ class Node
         this.childOf = childOf;
     }
 
-    public boolean isRoot(Node node)
-    {
+    public boolean isRoot(Node node) {
         if (node.childOf == null)
             return true;
-        else 
+        else
             return false;
-    }    
-}
-public class BST
-{
-    Node root;
-    public BST(int  data)
-    {
-        root = new Node(data, null , null , root);
     }
-    
-    public Node getRoot()
-    {
+}
+
+public class BST {
+    Node root;
+
+    public BST(int data) {
+        root = new Node(data, null, null, root);
+    }
+
+    public Node getRoot() {
         return root;
     }
 
-    private Node createRoot(int  data)
-    {
-        Node node = new Node(data , null, null, null);
+    private Node createRoot(int data) {
+        Node node = new Node(data, null, null, null);
         return node;
     }
 
-    public void addElement(int  data) throws IllegalArgumentException
-    {
+    public void addElement(int data) throws IllegalArgumentException {
         Node newNode = createRoot(data);
         Node parent = null;
         Node tempRoot = root;
-        while(tempRoot != null)
-        {
+        while (tempRoot != null) {
             parent = tempRoot;
-            if(data < tempRoot.getValue())
-                tempRoot  = tempRoot.getLeft();
-            else 
-                tempRoot  = tempRoot.getRight();
+            if (data < tempRoot.getValue())
+                tempRoot = tempRoot.getLeft();
+            else
+                tempRoot = tempRoot.getRight();
         }
-        if(data < parent.getValue())
-        {
+        if (data < parent.getValue()) {
             newNode.setParent(parent);
             parent.setLeft(newNode);
-        }
-        else if(data > parent.getValue())
-        {
+        } else if (data > parent.getValue()) {
             newNode.setParent(parent);
             parent.setRight(newNode);
-        }
-        else 
+        } else
             throw new IllegalArgumentException("Binary Tree cannot have duplicate value");
     }
 
-    void midOrderTraversal(Node root)
-    {
-        if(root == null)
+    void midOrderTraversal(Node root) {
+        if (root == null)
             return;
-        else{
+        else {
             midOrderTraversal(root.getLeft());
             System.out.println(root.getValue());
             midOrderTraversal(root.getRight());
         }
     }
 
-    int findSmallestElement(Node root)
-    {
-        if(root == null || root.getLeft() == null)
+    int findSmallestElement(Node root) {
+        if (root == null || root.getLeft() == null)
             return root.getValue();
-        else 
+        else
             return findSmallestElement(root.getLeft());
     }
 
-    int findLargestElement(Node root)
-    {
-        if(root == null || root.getRight() == null)
+    int findLargestElement(Node root) {
+        if (root == null || root.getRight() == null)
             return root.getValue();
         else
             return findLargestElement(root.getRight());
     }
 
-    int totalNodes(Node root)
-    {
-        if(root == null)
+    int totalNodes(Node root) {
+        if (root == null)
             return 0;
-        else 
+        else
             return totalNodes(root.getLeft()) + totalNodes(root.getRight()) + 1;
     }
 
+    int totalExternalNodes(Node root) {
+        if (root == null)
+            return 0;
+        if (root.getLeft() == null && root.getRight() == null)
+            return 1;
+        else
+            return totalExternalNodes(root.getLeft()) + totalExternalNodes(root.getRight());
 
+    }
 
+    Node search(Node root, int value) throws IllegalArgumentException {
+        if (root == null)
+            throw new IllegalArgumentException("no node found");
+        if (root.getValue() == value)
+            return root;
+        else {
+            if (root.getValue() > value)
+                return search(root.getLeft(), value);
+            else
+                return search(root.getRight(), value);
+        }
+    }
+
+    int totalInternalNodes(Node root) {
+        if (root == null || (root.getLeft() == null && root.getRight() == null))
+            return 0;
+        else
+            return totalInternalNodes(root.getLeft()) + totalInternalNodes(root.getRight()) + 1;
+    }
 
     public static void main(String[] args) {
         BST tree = new BST(10);
@@ -147,5 +159,6 @@ public class BST
         tree.addElement(110);
         tree.addElement(0);
         tree.addElement(75);
+        System.out.println(tree.totalInternalNodes(root));
     }
 }
