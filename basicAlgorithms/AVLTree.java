4dad53724c6222731d28acd04438c0ead1f3e311
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-05-01 16:01:29 +0530

    adding delete node functiolity

--- basicAlgorithms/AVLTree.java
+++ basicAlgorithms/AVLTree.java
@@ -33,10 +33,10 @@ public class AVLTree
                 left.right = unbalanced;
                 unbalanced.left = rightChildOfLeft;
 
-                unbalanced.height = max(height(unbalanced.left, unbalanced.right)) + 1;
+                unbalanced.height = max(height(unbalanced.left), height(unbalanced.right)) + 1;
                 //Now left node is balanced so
                 Node balanced = left; 
-                balanced.height = max(height(balanced.left, balanced.right));
+                balanced.height = max(height(balanced.left), height(balanced.right));
 
                 return balanced;
         }
@@ -51,17 +51,20 @@ public class AVLTree
                 right.left = unbalanced;
                 unbalanced.right = leftChildOfRight;
 
-                unbalanced.height = max(height(unbalanced.left, unbalanced.right));
+                unbalanced.height = max(height(unbalanced.left), height(unbalanced.right));
                 // right makes the tree balanced
                 Node balanced = right;
-                balanced.height = max(height(balanced.left, balanced.right));
+                balanced.height = max(height(balanced.left), height(balanced.right));
 
                 return unbalanced;
         }
 
         int getBalance(Node n)
         {
-                n == null ? 0 : height(n.left) - height(n.right);
+                if(n == null) 
+                        return  0; 
+                else
+                        return  height(n.left) - height(n.right);
         }
 
         Node insert(Node root, int key)
@@ -69,9 +72,9 @@ public class AVLTree
                 if(root == null) // root may be of sub tree or whole tree
                         return new Node(key);
                 if(key < root.key)
-                        node.left = insert(root.left , key);
+                        root.left = insert(root.left , key);
                 else if(key > root.key)
-                        node.right = insert(root.right, key);
+                        root.right = insert(root.right, key);
                 else 
                         return root;
 
@@ -85,17 +88,86 @@ public class AVLTree
                         return leftRotate(root);
                 if(balance > 1 && key > root.left.key)
                 {
-                        root.left = leftRotate(root.left)
+                        root.left = leftRotate(root.left);
                                 return rightRotate(root);
                 }
                 if(balance < -1 && key < root.right.key)
                 {
-                        root.right = rightRotate(node.right);
+                        root.right = rightRotate(root.right);
                         return leftRotate(root);
                 }
 
-                return node;
+                return root;
         }
+
+        Node minValueNode(Node node)
+        {
+                Node current = node;
+                while(current.left != null)
+                        current = current.left;
+                
+                return current;
+        }
+
+        Node deleteNode(Node root, int key)
+        {
+                if(root == null)
+                        return null;
+                if(key < root.key)
+                        root.left = deleteNode(root.left, key);
+                else if(key > root.key)
+                        root.right = deleteNode(root.right, key);
+                else 
+                {
+                        if((root.left == null) && (root.right == null))
+                        {
+                                Node temp = null;
+                                if(temp == root.left)
+                                        temp = root.right;
+                                else 
+                                        temp = root.left;
+                                
+                                if(temp == null)
+                                {
+                                        temp = root;
+                                        root = null;
+                                }
+                                else 
+                                      root = temp;
+                        }
+                        else 
+                        {
+                                Node temp = minValueNode(root); // in order 
+                                root.key = temp.key;
+                                root.right = deleteNode(root.right, temp.key);
+                        }
+
+                }
+                if(root == null)
+                        return null;
+                root.height = max(height(root.left), height(root.right)) + 1;
+                int balance = getBalance(root);
+
+                if(balance > 1 && getBalance(root.left) >= 0)
+                        return rightRotate(root);
+                
+                if(balance > 1 && getBalance(root.left) < 0 )
+                {
+                        root.left = leftRotate(root.left);
+                        return rightRotate(root);
+                }
+
+                if(balance < -1 && (getBalance(root.right) <= 0))
+                        return leftRotate(root);
+                
+                if(balance < -1 && (getBalance(root.right) > 0))
+                {
+                        root.right = rightRotate(root.right);
+                        return leftRotate(root);
+                }
+                return root;
+        }
+
 }
 
 
