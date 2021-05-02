b55b7b837ca27ba93872ab33d3ab268ad1e40386
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-05-02 14:24:45 +0530

    RedBlack tree

---
+++ basicAlgorithms/RedBlack.java
@@ -0,0 +1,105 @@
+import java.util.Stack;
+
+class Node
+{
+    int data;
+    Node left, right, parent;
+    boolean color;
+
+    Node(int data)
+    {
+        this.data = data;
+        left = right = parent = null;
+    }
+
+}
+public class RedBlack {
+    final static int RED = 0;
+    final static int  BLACK = 1;
+
+    private Node root;
+
+    void inOrderTraversal(Node root)
+    {
+        if(root == null)
+            return;
+        inOrderTraversal(root.left);
+        System.out.println(root.data);
+        inOrderTraversal(root.right);
+    }
+
+    Node BSTInsert(Node root, Node pt)
+    {
+        if(root == null)
+            return pt;
+        if(pt.data < root.data)
+        {
+            root.left = BSTInsert(root.left, pt);
+            root.left.parent = root;
+        }
+        else if(pt.data > root.data)
+        {
+            root.right = BSTInsert(root.right, pt);
+            root.right.parent = root;
+        }
+
+        return root;
+    }
+
+    int  height(Node root)
+    {
+        if(root == null)
+            return 0;
+        else 
+        {
+            int lheight = height(root.left);
+            int rheight = height(root.right);
+
+            if(lheight > rheight)
+                return (lheight + 1);
+            else
+                return  (rheight + 1);
+        }
+    }
+
+    void LevelOrderTraversal(Node root)
+    {
+        if(root == null)
+            return;
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.empty())
+        {
+            Node temp = stack.peek();
+            System.out.println(temp.data);
+            stack.pop();
+            if(temp.left != null)
+                stack.push(temp.left);
+            if(temp.right != null)
+                stack.push(temp.right);
+        }   
+    }
+
+    void LevelOrderTraversal2(Node root)
+    {
+        int h = height(root);
+        int i;
+        for(i=1;  i<=h; i++)
+        {
+            printGivenLevel(root, i);
+        }
+    }
+
+    void printGivenLevel(Node root, int level)
+    {
+        if(root == null)
+            return;
+        if(level == 1)
+            System.out.print(root.data);
+        else if(level > 1)
+        {
+            printGivenLevel(root.left, level - 1);
+            printGivenLevel(root.right,level - 1);
+        }
+    }
+}
