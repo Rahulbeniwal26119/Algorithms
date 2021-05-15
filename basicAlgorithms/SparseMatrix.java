bab6ce3c8866c7a355fd4b805a0fa7fffe4832f3
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-05-15 13:58:22 +0530

    adding add functionlity for sparse matrix

--- basicAlgorithms/SparseMatrix.java
+++ basicAlgorithms/SparseMatrix.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class SparseMatrix
 {
+	Scanner in = new Scanner(System.in);
 		public static class Element 
 		{
 				int i , j , x;
@@ -19,7 +20,6 @@ public class SparseMatrix
 
 		void create(SparseMatrix e)
 		{
-				Scanner in = new Scanner(System.in);
 				System.out.print("Enter dimensions");
 				e.numOfRows = in.nextInt();
 				e.numOfCols = in.nextInt();
@@ -48,14 +48,52 @@ public class SparseMatrix
 				}
 		}
 
+		static SparseMatrix add(SparseMatrix s1 , SparseMatrix s2)
+		{
+				SparseMatrix sum = new SparseMatrix();
+				sum.elementPointer = new Element[s1.numOfNonZeroElement + s2.numOfNonZeroElement];
+				int i,j,k;
+				i=j=k=0;
+				while(i < s1.numOfNonZeroElement && j<s2.numOfNonZeroElement)
+				{
+						if(s1.elementPointer[i].i < s2.elementPointer[j].i)
+								sum.elementPointer[k++] = s1.elementPointer[i++];
+						else if(s1.elementPointer[i].i > s2.elementPointer[j].i)
+							sum.elementPointer[k++] = s2.elementPointer[j++];
+						else
+						{
+							if(s1.elementPointer[i].j < s2.elementPointer[j].j)
+								sum.elementPointer[k++] = s2.elementPointer[i++];
+							else if(s1.elementPointer[i].j > s2.elementPointer[j].j)
+								sum.elementPointer[k++] = s2.elementPointer[j++];
+							else 
+							{
+								sum.elementPointer[k] = s1.elementPointer[i];
+								sum.elementPointer[k++].x = s1.elementPointer[i++].x + s2.elementPointer[j++].x;
+							}
+						} 
+					}
+					for(; i<s1.numOfNonZeroElement; i++)
+						sum.elementPointer[k++] = s1.elementPointer[i];
+					for(;j<s2.numOfNonZeroElement; j++)
+						sum.elementPointer[k++] = s2.elementPointer[j];
+					sum.numOfRows = s1.numOfRows;
+					sum.numOfCols= s1.numOfCols;
+					sum.numOfNonZeroElement = k;
+
+					return sum;
+		}
+
 
 
 
 
 		public static void main(String []args){
-				SparseMatrix matrix = new SparseMatrix();
-				matrix.create(matrix);
-				matrix.display(matrix);
+				SparseMatrix matrix1 = new SparseMatrix();
+				SparseMatrix matrix2 = new SparseMatrix(); 
+				matrix1.create(matrix1);
+				matrix2.create(matrix2);
+				matrix1.display(add(matrix1, matrix2));
 		}
 }
 
