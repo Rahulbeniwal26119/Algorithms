5a1010a239127325d963de8c0459c36f6898d8d2
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-06-03 17:44:26 +0530

    adding Insertion sort

--- basicAlgorithms/sorting/Main.java
+++ basicAlgorithms/sorting/Main.java
@@ -1,9 +1,11 @@
-package basicAlgorithms.sorting;
+package sorting;
 
 public class Main{
 		public static void main(String []args){
-				int array[] = {5,4,3,2,1,1};
-				BubbleSort.bubbleSort(array);
+				int array[] = {1,1,2,3,4,5,6};
+				// BubbleSort.bubbleSort(array);
+				// Insertion is best choice for linked list
+				InsertionSort.insertionSort(array);
 				for(var ele : array)
 						System.out.println(ele);
 		}
