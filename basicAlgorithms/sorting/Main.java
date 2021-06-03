bab9e314e6b2889f5b5c9dd663245d909c36cade
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-06-03 16:07:35 +0530

    Main file to run code

---
+++ basicAlgorithms/sorting/Main.java
@@ -0,0 +1,11 @@
+package basicAlgorithms.sorting;
+
+public class Main{
+		public static void main(String []args){
+				int array[] = {5,4,3,2,1,1};
+				BubbleSort.bubbleSort(array);
+				for(var ele : array)
+						System.out.println(ele);
+		}
+}
+
