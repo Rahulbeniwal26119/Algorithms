d78bc96c402fe69250549c2c71556b5b0e584d6c
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-06-04 07:39:43 +0530

    adding quick sort

--- basicAlgorithms/sorting/Main.java
+++ basicAlgorithms/sorting/Main.java
@@ -2,11 +2,12 @@ package sorting;
 
 public class Main{
 		public static void main(String []args){
-				int array[] = {1,1,2,3,4,5,6};
+				int array[] = {10,5,4,1,2,2,3,4,5};
 				// BubbleSort.bubbleSort(array);
 				// Insertion is best choice for linked list
 				// InsertionSort.insertionSort(array);
-				SelectionSort.selectionSort(array);
+				// SelectionSort.selectionSort(array);
+				QuickSort.quickSort(array, 0, array.length-1);
 				for(var ele : array)
 						System.out.println(ele);
 		}
