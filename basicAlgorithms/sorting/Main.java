d7af11892e22de199d8c5faafdd60851b81621d0
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-06-05 08:11:53 +0530

    testing algo with different input set

--- basicAlgorithms/sorting/Main.java
+++ basicAlgorithms/sorting/Main.java
@@ -2,13 +2,13 @@ package sorting;
 
 public class Main{
 		public static void main(String []args){
-				int array[] = {10,5,4,1,2,2,3,4,5};
+				int array[] = {5,7,9,10,11,12,13,16,24,3};
 				// BubbleSort.bubbleSort(array);
 				// Insertion is best choice for linked list
 				// InsertionSort.insertionSort(array);
 				// SelectionSort.selectionSort(array);
 				   IterativeMergeSort.iterativeMergeSort(array);
-				QuickSort.quickSort(array, 0, array.length-1);
+				// QuickSort.quickSort(array, 0, array.length-1);
 				for(var ele : array)
 						System.out.println(ele);
 		}
