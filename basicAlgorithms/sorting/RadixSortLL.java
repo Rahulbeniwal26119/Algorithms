ac024b681765cf77c8ed8e15b83cd9253d28f144
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-06-06 10:31:49 +0530

    adding radix sort by LinkedList

---
+++ basicAlgorithms/sorting/RadixSortLL.java
@@ -0,0 +1,57 @@
+package sorting;
+
+import java.io.*;
+import java.net.Inet4Address;
+import java.util.*;
+
+public class RadixSortLL {
+	static int getMax(int arr[]) {
+		int max = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max)
+				max = arr[i];
+		}
+		return max;
+	}
+
+	static int getPasses(int max, int numbers) {
+		int count = 0;
+		while (max != 0) {
+			count++;
+			max /= numbers;
+		}
+		return count;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void radixSortLL(int arr[]) {
+		LinkedList<Integer> outputArray[] = new LinkedList[10];
+		for (int i = 0; i < 10; i++) {
+			outputArray[i] = new LinkedList<Integer>();
+		}
+
+		int max = getMax(arr);
+		int passes = getPasses(max, 10);
+		int exp = 10;
+		int divideFactor = 1;
+		while (passes > 0) {
+			--passes;
+			for (var num : arr) {
+				var index = (num / divideFactor) % exp;
+				outputArray[index].add(num);
+			}
+			int i = 0;
+			for (var list : outputArray) {
+				if (!list.isEmpty()) {
+					for (int ele : list) {
+						arr[i] = ele;
+						i++;
+					}
+				}
+			}
+			for (var ll : outputArray)
+				ll.clear();
+			divideFactor *= 10;
+		}
+	}
+}
