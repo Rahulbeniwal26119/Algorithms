9eaec46e44ab042cc76bada4d3dc6a435d955b4c
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-06-06 11:02:32 +0530

    adding Bucker Sort

--- basicAlgorithms/sorting/Main.java
+++ basicAlgorithms/sorting/Main.java
@@ -1,19 +1,19 @@
 package sorting;
 
-public class Main{
-		public static void main(String []args){
-				int array[] = {5,7,9,10,11,12,13,16,24,3};
-				// BubbleSort.bubbleSort(array);
-				// Insertion is best choice for linked list
-				// InsertionSort.insertionSort(array);
-				// SelectionSort.selectionSort(array);
-				// IterativeMergeSort.iterativeMergeSort(array); 
-				// RecursiveMergeSort.recursiveMergeSort(array, 0 , array.length -1);
-				// QuickSort.quickSort(array, 0, array.length-1);
-				// CountSort.countSort(array);
-				   RadixSortLL.radixSortLL(array);
-				for(var ele : array)
-						System.out.println(ele);
-		}
-}
+public class Main {
 
+  public static void main(String[] args) {
+    int array[] = { 5, 7, 9, 10, 11, 12, 13, 16, 24, 3 };
+    // BubbleSort.bubbleSort(array);
+    // Insertion is best choice for linked list
+    // InsertionSort.insertionSort(array);
+    // SelectionSort.selectionSort(array);
+    // IterativeMergeSort.iterativeMergeSort(array);
+    // RecursiveMergeSort.recursiveMergeSort(array, 0 , array.length -1);
+    // QuickSort.quickSort(array, 0, array.length-1);
+    // CountSort.countSort(array);
+    // RadixSortLL.radixSortLL(array);
+       BucketSort.bucketSort(array);
+    for (var ele : array) System.out.println(ele);
+  }
+}
