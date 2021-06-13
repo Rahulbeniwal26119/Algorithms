862217a8216ea5873046f5983eb0d9cfc800c1ec
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-06-13 19:37:14 +0530

    adding heap sort

--- crackingCoadingInterview/LinkedList/GeneralLL.java
+++ crackingCoadingInterview/LinkedList/GeneralLL.java
@@ -19,12 +19,43 @@ class GeneralLL extends LinkedList{
 				return index;
 		}
 
-		public void printLLReversed(Node head){
+		public static  void printLLReversed(Node head){
 				if(head!=null){
 						System.out.println(head.data);
 						printLLReversed(head.next);
 				}
 		}
+
+		Node  addLists(Node l1, Node l2, int carry){
+				if(l1 == null && l2 == null && carry == 0)
+						return null;
+				LinkedList result = new LinkedList(0);
+				Node head = result.head;
+
+				int value = carry;
+
+				if(l1 != null){
+						value+=l1.data;
+				}
+				if(l2 != null){
+						value+=l2.data;
+				}
+				head.data = value % 10;
+				if(l1 != null || l2 != null)
+				{
+						// System.out.println(l1.data + " " + l2.data);
+						Node more = addLists(l1 == null ? null : l1.next, 
+										l2 == null ? null : l2.next , value > 10 ? 1 : 0);
+						// System.out.println(l1.data + " " + l2.data + "at last");
+						// if(more != null){
+						//System.out.println(more.data + " data ");
+						printLLReversed(head);
+						result.appendToLast(more.data);
+						//}
+				}
+				return result.head;
+		}
+
 
 
 		boolean deleteNode(Node n){
@@ -57,18 +88,16 @@ class GeneralLL extends LinkedList{
 		}
 
 		public static void main(String []args){
-				GeneralLL ll = new GeneralLL(10);
-				GeneralLL ld = new GeneralLL(10);
-				ll.appendToLast(15);
-				ll.appendToLast(9);
-				ll.appendToLast(8);
+				GeneralLL ll = new GeneralLL(6);
+				GeneralLL ld = new GeneralLL(2);
+				ll.appendToLast(1);
 				ll.appendToLast(7);
-				ll.appendToLast(12);
-				ll.appendToLast(13);
-				ll.appendToLast(14);
-				ld.head = ll.partition(ll.head,12);
+				ld.appendToLast(9);
+				ld.appendToLast(5);
+
+				Node result = ld.addLists(ll.head, ld.head, 0);
 				//ll.printLLReversed(ll.head);
-				ll.printLLReversed(ll.head);
+				// ll.printLLReversed(result);
 		}
 }
 
