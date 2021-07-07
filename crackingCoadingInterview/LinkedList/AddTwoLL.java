90b29063a17bc2b78b82dcf1026b609cb8236135
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-07-07 09:16:04 +0530

    addition two linked list

--- crackingCoadingInterview/LinkedList/AddTwoLL.java
+++ crackingCoadingInterview/LinkedList/AddTwoLL.java
@@ -1,1 +1,22 @@
+		Node addLists(Node l1, Node l2, int carry){
+
+		if( l1 == null && l2 == null && carry == 0)
+				return null;
+
+		Node result = new Node(0); // providing default value for a node  
+		int value  = carry;
+
+		if(l1 != null)
+				value += l1.data;
+		if(l2 != null)
+				value += l2.data;
+
+		result.data = value % 10;
+
+		if(l1 != null || l2 != null){
+				Node more = addLists(l1 == null ? null : l1.next, l2 == null ? null : l2.next, value >= 10 ? 1 : 0);
+		result.next = more;
+		}
+		return result;
+}
 
