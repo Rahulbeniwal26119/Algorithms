6c2ed32628116b12a6b409aca3031139eb48c4cf
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-07-13 20:20:12 +0530

    adding fundamental stack class

---
+++ crackingCoadingInterview/Stack_Queue/Stack.java
@@ -0,0 +1,37 @@
+public class Stack<T>{
+		private static class StackNode<T>{
+				private T data;
+				private StackNode<T> next;
+
+				public StackNode(T data){
+						this.data = data;
+				}
+		}
+
+		private StackNode<T> top;
+
+		public T pop() throws EmptyStackException{
+				if(top == null)
+						throw new EmptyStackException();
+				T item = top.data;
+				top = top.next;
+				return item;
+		}
+
+		public void push(T data){
+				StackNode<T> t = new StackNode<>(data);
+				t.next = top;
+				top = t;
+		}
+
+		public T peek() throws EmptyStackException{
+				if(top == null)
+						throw new EmptyStackException();
+				return top.data;
+		}
+
+		public boolean isEmpty(){
+				return top == null;
+		}
+}
+
