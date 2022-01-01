7aaef0eb3fe5ac7049d9ab29704ae715edf7e47b
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2022-01-02 00:44:41 +0530

    adding divide algo quad

---
+++ basicAlgorithms/divide_algo.py
@@ -0,0 +1,13 @@
+def divide(x,y):
+    if x == 0: return (0,0)
+    (q,r) = divide(x//2,y)
+    q = 2*q
+    r = 2*r
+    if x%2 != 0:
+        r = r+1
+    if r >= y:
+        r = r-y
+        q = q + 1
+    return (q,r)
+
+print(divide(101,10))
